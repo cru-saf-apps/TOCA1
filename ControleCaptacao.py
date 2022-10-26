b184029407215767340597b83a9eba912a5f3818
cru-saf-apps <116762584+cru-saf-apps@users.noreply.github.com>
2022-10-26 17:44:38 -0300

    Update ControleCaptacao.py

--- ControleCaptacao.py
+++ ControleCaptacao.py
@@ -2,6 +2,6 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-base = pd.read_csv('Planilha Geral - Captação TOCA I1.csv',sep=';',decimal=',')[3:]
+base = pd.read_csv('Planilha Geral - Captação TOCA I1.csv',sep=';',decimal=',')[2:]
 
 st.write(base)
