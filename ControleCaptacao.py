ab63ea9694ce4db1de9c0fc9194cfc2b15902746
cru-saf-apps <116762584+cru-saf-apps@users.noreply.github.com>
2022-10-26 22:10:26 -0300

    Update ControleCaptacao.py

--- ControleCaptacao.py
+++ ControleCaptacao.py
@@ -4,3 +4,8 @@ import numpy as np
 
 base = pd.read_csv('Planilha Geral - Captação TOCA I1.csv',sep=';',decimal=',')
 st.write(base)
+
+st.subheader("O que deseja fazer?")
+
+acao = st.radio("Selecione o que deseja fazer:",options = ['Adicionar atleta','Ver histórico de atleta','Editar atleta'])
+
