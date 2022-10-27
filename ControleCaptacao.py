97a5f539f18230866f35d7515b9ec328604c679a
cru-saf-apps <116762584+cru-saf-apps@users.noreply.github.com>
2022-10-26 22:43:42 -0300

    Update ControleCaptacao.py

--- ControleCaptacao.py
+++ ControleCaptacao.py
@@ -10,8 +10,25 @@ st.subheader("O que deseja fazer?")
 acao = st.radio("Selecione o que deseja fazer:",options = ['Adicionar atleta','Ver histórico de atleta','Editar atleta'])
 
 if acao == 'Adicionar atleta':
-  nome = st.text_input("Nome")
-  datanasc = st.date_input("Data de nascimento")
-  tel = st.text_input("Contato de telefone")
-  cat = st.radio("Categoria de chegada (Sub-...)",options=[6,7,8,9,10,11,12,13,14,15,17,20])
-  pos = st.radio("Posição",options=['GOL','LD','ZAG','LE','VOL','MEI','EXT','ATA'])
+  col1, col2 = st.columns(2)
+  
+  with col1:
+    st.subheader("Informações pessoais")
+    
+    nome = st.text_input("Nome")
+    datanasc = st.date_input("Data de nascimento")
+    tel = st.text_input("Contato de telefone")
+    cat = st.radio("Categoria de chegada (Sub-...)",options=[6,7,8,9,10,11,12,13,14,15,17,20])
+    pos = st.radio("Posição",options=['GOL','LD','ZAG','LE','VOL','MEI','EXT','ATA'])
+    
+  with col2:
+    st.subheader("Origem do jogador")
+    
+    indicador = st.text_input("Indicador (Pessoa de dentro do clube)")
+    visu = st.radio("In loco / Contato externo",options=["In loco","Contato externo"])
+    if visu == "Contato externo":
+      contato = st.text_input("Contato externo")
+    else:
+      contato = "-"
+    origem = st.text_input("Origem do jogador (Último clube / escolinha / projeto)")
+   
