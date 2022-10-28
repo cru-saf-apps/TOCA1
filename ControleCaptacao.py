322926d2db8001e96234bafb34869604351b1183
cru-saf-apps <116762584+cru-saf-apps@users.noreply.github.com>
2022-10-28 15:48:02 -0300

    Update ControleCaptacao.py

--- ControleCaptacao.py
+++ ControleCaptacao.py
@@ -111,4 +111,4 @@ elif acao == 'Editar atleta':
   st.write(base[base.Nome.isin(lista_results)][['Nome','Data Nascimento','Posição']])
   
   
-baixar = st.download_button("Baixar base",'Planilha Geral - TOCA 1.csv')
+baixar = st.download_button("Baixar base",base)
