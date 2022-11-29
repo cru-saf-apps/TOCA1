57ccc1af0c12132a7f0d2af4814b1594ec4ef732
cru-saf-apps <116762584+cru-saf-apps@users.noreply.github.com>
2022-11-29 14:25:39 -0300

    Create Dashboard_Captação.py

---
+++ pages/Dashboard_Captação.py
@@ -0,0 +1,36 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+from gspread_pandas import Spread,Client
+from google.oauth2 import service_account
+
+st.set_page_config(
+    page_title="Dashboard Captação - CEC",
+    page_icon="✅",
+    layout="wide",
+)
+
+st.title("Dashboard Captação - CEC")
+
+def load_spreadsheet(spreadsheet_name):
+    worksheet = sh.worksheet(spreadsheet_name)
+    df = pd.DataFrame(worksheet.get_all_records())
+    return df
+  
+scope = ['https://spreadsheets.google.com/feeds',
+         'https://www.googleapis.com/auth/drive']
+
+
+credentials = service_account.Credentials.from_service_account_info(
+    st.secrets["gcp_service_account"],
+    scopes=scope
+)
+
+client = Client(scope = scope, creds = credentials)
+
+spreadsheet_name = "MONITORAMENTO"
+spread = Spread(spreadsheet_name, client = client)
+sh = client.open(spreadsheet_name)
+base = load_spreadsheet(spreadsheet_name)
+
+st.write(base)
