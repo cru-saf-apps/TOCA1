9866a7d0249d79dac887f27bdc1e68de5bcca9bb
cru-saf-apps <116762584+cru-saf-apps@users.noreply.github.com>
2022-11-29 16:33:46 -0300

    Update Agenda_Semanal.py

--- pages/Agenda_Semanal.py
+++ pages/Agenda_Semanal.py
@@ -14,7 +14,9 @@ def load_spreadsheet(spreadsheet_name):
     worksheet = sh.worksheet(spreadsheet_name)
     df = pd.DataFrame(worksheet.get_all_records())
     return df
-  
+
+
+
 scope = ['https://spreadsheets.google.com/feeds',
          'https://www.googleapis.com/auth/drive']
 
@@ -29,62 +31,132 @@ client = Client(scope = scope, creds = credentials)
 spreadsheet_name = "MONITORAMENTO"
 spread = Spread(spreadsheet_name, client = client)
 sh = client.open(spreadsheet_name)
-base = load_spreadsheet(spreadsheet_name)
-
-base['DATA INÍCIO'] = pd.to_datetime(base['DATA INÍCIO'],infer_datetime_format = True)
-base['DATA FIM'] = pd.to_datetime(base['DATA FIM'],infer_datetime_format = True)
-base['DATA NASCIMENTO'] = pd.to_datetime(base['DATA NASCIMENTO'],infer_datetime_format = True)
+monit = load_spreadsheet(spreadsheet_name)
 
 
-semana = st.date_input('Selecione a semana para visualizar monitoramentos',dt.date.today()).strftime('%m/%d/%Y')
-
-base_print = base[base['DATA INÍCIO'] == semana]
-
-if base_print.empty:
-    st.write('Não há atletas monitorados na data selecionada.')
-
-else:
-    anos = st.multiselect('Selecione a categoria',pd.unique(base_print.ANO))
-    lista_anos = []
-    for ano in anos:
-        lista_anos.append(ano)
-
-    base_print = base_print[base_print.ANO.isin(lista_anos)]
-
-    pdf = FPDF()
-    pdf.add_page()
-    pdf.set_font('Arial','B',16)
-    try:
-        pdf.cell(100,10,"Agenda de Monitorados em "+str(base_print['DATA INÍCIO'].tolist()[0].strftime('%d/%m/%Y')),ln=1,border='B')
-        pdf.cell(40,10," ",ln=1)
-    except:
-        st.write('Favor selecionar uma semana válida e anos válidos')
+spreadsheet_name = "AVALIAÇÃO"
+spread = Spread(spreadsheet_name, client = client)
+sh = client.open(spreadsheet_name)
+aval = load_spreadsheet(spreadsheet_name)
 
 
-    for ano in pd.unique(base_print.ANO):
+monit['DATA INÍCIO'] = pd.to_datetime(monit['DATA INÍCIO'],infer_datetime_format = True)
+monit['DATA FIM'] = pd.to_datetime(monit['DATA FIM'],infer_datetime_format = True)
+monit['DATA NASCIMENTO'] = pd.to_datetime(monit['DATA NASCIMENTO'],infer_datetime_format = True)
 
-        base_ano = base_print[base_print.ANO == ano].reset_index(drop=True)
+aval['DATA INÍCIO'] = pd.to_datetime(aval['DATA INÍCIO'],infer_datetime_format = True)
+aval['DATA FIM'] = pd.to_datetime(aval['DATA FIM'],infer_datetime_format = True)
+aval['DATA NASCIMENTO'] = pd.to_datetime(aval['DATA NASCIMENTO'],infer_datetime_format = True)
 
-        pdf.set_font('Arial','B',14)
-        pdf.cell(15,10,str(ano),ln=1,border='B')    
 
-        comp = len(base_ano)
+opcao = st.radio('Deseja visualizar Avaliações ou Monitoramentos?',options=['Avaliações','Monitoramentos'])
 
-        t = 0
-        while t < comp:
+if opcao == 'Monitoramentos':
 
-          pdf.set_font('Arial','',12)
-          pdf.cell(40, 10, base_ano['NOME COMPLETO'][t],ln=0,border='B')
+    semana = st.date_input('Selecione a semana para visualizar monitoramentos',dt.date.today()).strftime('%m/%d/%Y')
 
-          pdf.cell(40, 10, base_ano['POSIÇÃO'][t],ln=0,border='B')
+    monit_print = monit[monit['DATA INÍCIO'] == semana]
 
-          pdf.cell(40, 10, str(base_ano['DATA NASCIMENTO'][t].strftime('%d/%m/%Y')),ln=1,border='B')
-          pdf.cell(40, 10, " ",ln=1)
+    if monit_print.empty:
+        st.write('Não há atletas monitorados na data selecionada.')
 
-          t+=1
+    else:
+        anos = st.multiselect('Selecione a categoria',pd.unique(monit_print.ANO))
+        lista_anos = []
+        for ano in anos:
+            lista_anos.append(ano)
 
-    export_as_pdf = st.button("Exportar")
+        monit_print = monit_print[monit_print.ANO.isin(lista_anos)]
 
-    if export_as_pdf:
-        html = create_download_link(pdf.output(dest="S").encode("latin-1"), "Agenda de Monitoramentos em "+str(base_print['DATA INÍCIO'].tolist()[0].strftime('%d/%m/%Y')))
-        st.markdown(html, unsafe_allow_html=True)
+        pdf = FPDF()
+        pdf.add_page()
+        pdf.set_font('Arial','B',16)
+        try:
+            pdf.cell(100,10,"Agenda de Monitorados em "+str(monit_print['DATA INÍCIO'].tolist()[0].strftime('%d/%m/%Y')),ln=1,border='B')
+            pdf.cell(40,10," ",ln=1)
+        except:
+            st.write('Favor selecionar uma semana válida e anos válidos')
+
+
+        for ano in pd.unique(monit_print.ANO):
+
+            monit_ano = monit_print[monit_print.ANO == ano].reset_index(drop=True)
+
+            pdf.set_font('Arial','B',14)
+            pdf.cell(15,10,str(ano),ln=1,border='B')    
+
+            comp = len(monit_ano)
+
+            t = 0
+            while t < comp:
+
+              pdf.set_font('Arial','',12)
+              pdf.cell(40, 10, monit_ano['NOME COMPLETO'][t],ln=0,border='B')
+
+              pdf.cell(40, 10, monit_ano['POSIÇÃO'][t],ln=0,border='B')
+
+              pdf.cell(40, 10, str(monit_ano['DATA NASCIMENTO'][t].strftime('%d/%m/%Y')),ln=1,border='B')
+              pdf.cell(40, 10, " ",ln=1)
+
+              t+=1
+
+        export_as_pdf = st.button("Exportar")
+
+        if export_as_pdf:
+            html = create_download_link(pdf.output(dest="S").encode("latin-1"), "Agenda de Monitoramentos em "+str(monit_print['DATA INÍCIO'].tolist()[0].strftime('%d/%m/%Y')))
+            st.markdown(html, unsafe_allow_html=True)
+
+elif opcao == 'Avaliações':
+    
+    semana = st.date_input('Selecione a semana para visualizar avaliações',dt.date.today()).strftime('%m/%d/%Y')
+
+    aval_print = aval[aval['DATA INÍCIO'] == semana]
+
+    if aval_print.empty:
+        st.write('Não há atletas monitorados na data selecionada.')
+
+    else:
+        anos = st.multiselect('Selecione a categoria',pd.unique(aval_print.ANO))
+        lista_anos = []
+        for ano in anos:
+            lista_anos.append(ano)
+
+        aval_print = aval_print[aval_print.ANO.isin(lista_anos)]
+
+        pdf = FPDF()
+        pdf.add_page()
+        pdf.set_font('Arial','B',16)
+        try:
+            pdf.cell(100,10,"Agenda de Monitorados em "+str(aval_print['DATA INÍCIO'].tolist()[0].strftime('%d/%m/%Y')),ln=1,border='B')
+            pdf.cell(40,10," ",ln=1)
+        except:
+            st.write('Favor selecionar uma semana válida e anos válidos')
+
+
+        for ano in pd.unique(aval_print.ANO):
+
+            aval_ano = aval_print[aval_print.ANO == ano].reset_index(drop=True)
+
+            pdf.set_font('Arial','B',14)
+            pdf.cell(15,10,str(ano),ln=1,border='B')    
+
+            comp = len(aval_ano)
+
+            t = 0
+            while t < comp:
+
+              pdf.set_font('Arial','',12)
+              pdf.cell(40, 10, aval_ano['NOME COMPLETO'][t],ln=0,border='B')
+
+              pdf.cell(40, 10, aval_ano['POSIÇÃO'][t],ln=0,border='B')
+
+              pdf.cell(40, 10, str(aval_ano['DATA NASCIMENTO'][t].strftime('%d/%m/%Y')),ln=1,border='B')
+              pdf.cell(40, 10, " ",ln=1)
+
+              t+=1
+
+        export_as_pdf = st.button("Exportar")
+
+        if export_as_pdf:
+            html = create_download_link(pdf.output(dest="S").encode("latin-1"), "Agenda de Monitoramentos em "+str(monit_print['DATA INÍCIO'].tolist()[0].strftime('%d/%m/%Y')))
+            st.markdown(html, unsafe_allow_html=True)
